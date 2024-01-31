06600a3ad481c5644ffcd4100f69b73e127b29b5
Karim Ahmed <devKarimAhmed@gmail.com>
2024-01-31 18:42:10 +0200

    add animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,18 +1,60 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({super.key});
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    // Start the animation
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the animation controller
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image(image: AssetImage(AssetsData.logo)),
-         SizedBox(height: 16,),
-        Text('Read Free Books' , textAlign: TextAlign.center,),
+        const Image(image: AssetImage(AssetsData.logo)),
+        const SizedBox(
+          height: 8,
+        ),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, _) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Read Free Books',
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
+        ),
       ],
     );
   }
