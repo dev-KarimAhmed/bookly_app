b35df2cba474cce2c99778a3f2609cb4f29bea2b
Karim Ahmed <devKarimAhmed@gmail.com>
2024-02-01 14:56:24 +0200

    create custom bokoks list view

--- lib/features/home/presentation/views/widgets/custom_list_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_item.dart
@@ -5,19 +5,16 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .23,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: const DecorationImage(
-                fit: BoxFit.fill,
-                image: NetworkImage(
-                    'https://img.freepik.com/free-photo/3d-rendered-illustration-tiger-cartoon-character-isolated-white-background_1142-36687.jpg?size=626&ext=jpg')),
-          ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.red,
+          image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(
+                  'https://img.freepik.com/free-photo/3d-rendered-illustration-tiger-cartoon-character-isolated-white-background_1142-36687.jpg?size=626&ext=jpg')),
         ),
       ),
     );
