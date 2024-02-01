d8b439dbd4bf7fd740f593f82a711790ee4280bd
Karim Ahmed <devKarimAhmed@gmail.com>
2024-02-01 15:55:33 +0200

    add google fonts

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const mediumTitle = TextStyle(
-    fontSize: 19,
+    fontSize: 18,
     fontWeight: FontWeight.w600,
   );
 }
