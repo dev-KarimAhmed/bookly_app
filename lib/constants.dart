2a011ea700d1e24ca1fbc763dea2429e1861779e
Karim Ahmed <devKarimAhmed@gmail.com>
2024-06-20 17:17:21 +0300

    Register book entity adapter and open featured box and init hive

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,5 @@ const Duration kTransitionDuration = Duration(milliseconds: 250);
 const String kGTSectraFine = 'GT Sectra Fine';
 const String kMyImage =
     'https://img.freepik.com/premium-photo/adorable-3d-cat-characters-wear-cute-funny-colorful-clothes_784625-1152.jpg?w=740';
+
+const String kFeaturedBox = 'featured_box';    
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox<BookEntity>(kFeaturedBox);
   setupServiceLocator();
   runApp(const BooklyApp());
 }
@@ -20,8 +26,7 @@ class BooklyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
-        textTheme:
-            GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
     );
   }
