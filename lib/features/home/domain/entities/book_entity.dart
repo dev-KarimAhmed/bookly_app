62d80a347e56c6d3a56161006fd2e4c051c302ed
Karim Ahmed <devKarimAhmed@gmail.com>
2024-06-20 14:46:26 +0300

    make BookModel extends BookEntity

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,10 +1,18 @@
 class BookEntity {
-  final String image;
-  final String title;
-  final String author;
-  final num price;
-  final num rating;
+  final String? image;
+  final String? bookId;
+  final String? title;
+  final String? author;
+  final num? price;
+  final num? rating;
 
-  BookEntity({required this.image, required this.title, required this.author, required this.price, required this.rating});
-  
+  BookEntity(
+      {
+      required this.image,
+      required this.title,
+      required this.author,
+      required this.price,
+      required this.rating,
+      required this.bookId,
+      });
 }
