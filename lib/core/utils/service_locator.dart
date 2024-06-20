75d53fe57773d13679774e64b2c5b95964fb549d
Karim Ahmed <devKarimAhmed@gmail.com>
2024-06-20 20:25:52 +0300

    create bloc provider and get_it

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,4 +1,7 @@
 import 'package:bookly_app/core/utils/api_services.dart';
+import 'package:bookly_app/features/home/Data/data_sources/home_local_data_source.dart';
+import 'package:bookly_app/features/home/Data/data_sources/home_remote_data_source.dart';
+import 'package:bookly_app/features/home/Data/repos/home_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -6,5 +9,9 @@ final getIt = GetIt.instance;
 
 void setupServiceLocator() {
   getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
-  // getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiServices>()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    remoteDataSource:
+        HomeRemoteDataSourceImpl(apiServices: getIt.get<ApiServices>()),
+    localDataSource: HomeLocalDataSourceImpl(),
+  ));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/Data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/presentation/manger/fetch_feature_books_cubit/fetch_feature_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manger/fetch_newest_books_cubit/fetch_newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,12 +26,24 @@ class BooklyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FetchNewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => FetchFeatureBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
